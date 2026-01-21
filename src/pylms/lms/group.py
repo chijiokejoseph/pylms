a4ff02476138a13828e6c81eb605dbccc49de315
chijiokejoseph <chijiokejoseph7@proton.me>
2026-01-21 13:39:07 +0100

    update pandas to polars semantics in group.py and other fixes
    This commit updates the pandas semantics in group.py of the 'lms' package to polars semantics while updating the error check for update forms from checking that at least one update form is recorded to checking that all update forms are recorded

--- src/pylms/lms/group.py
+++ src/pylms/lms/group.py
@@ -1,11 +1,10 @@
 from pathlib import Path
-from typing import cast
 
-import numpy as np
+import polars as pl
 
 from ..cli import input_bool, input_num
 from ..constants import GROUP, NAME, SERIAL
-from ..data import DataStore, DataStream
+from ..data import DataStore, write
 from ..errors import Result, Unit, eprint
 from ..history import History, set_group
 from ..info import print_info
@@ -13,16 +12,16 @@ from ..paths import get_group_dir, get_group_path
 
 
 def group(ds: DataStore, history: History) -> Result[Unit]:
-    if len(history.recorded_update_forms) == 0 and len(history.update_forms) > 0:
-        msg = "You need to record at least one of the update forms you created for the cohort before grouping students\n"
+    if len(history.recorded_update_forms) < len(history.update_forms):
+        msg = "You need to record all of the update forms you created for the cohort before grouping students\n"
         eprint(msg)
         return Result.err(msg)
 
-    group_data_path: Path = get_group_path()
+    groups_path: Path = get_group_path()
     group_dir: Path = get_group_dir()
-    if group_data_path.exists():
+    if groups_path.exists():
         print_info(
-            f"You have already grouped students. \nPlease check the path {group_data_path.resolve()} for the previous grouping operation you performed."
+            f"You have already grouped students. \nPlease check the path {groups_path.resolve()} for the previous grouping operation you performed."
         )
         prompt: str = "Do you wish to update or redo the grouping?"
 
@@ -35,36 +34,32 @@ def group(ds: DataStore, history: History) -> Result[Unit]:
             return Result.unit()
 
     group_dir.mkdir(exist_ok=True)
-    pretty_data: pl.DataFrame = ds.pretty()
+    pretty = ds.pretty()
 
     msg: str = "Please enter the number of groups [Must be between 3 - 100]: "
-    num_result = input_num(msg, 1, lambda x: 3 <= x <= 100)
+    num_groups = input_num(msg, 1, lambda x: 3 <= x <= 100)
 
-    if num_result.is_err():
-        return num_result.propagate()
+    if num_groups.is_err():
+        return num_groups.propagate()
 
-    num_groups = num_result.unwrap()
+    num_groups = num_groups.unwrap()
     groups: list[int] = [
         num_groups if serial % num_groups == 0 else serial % num_groups
-        for serial in range(1, pretty_data.shape[0] + 1)
+        for serial in range(1, pretty.shape[0] + 1)
     ]
-    group_data: pl.DataFrame = pretty_data.loc[:, [SERIAL, NAME]]
-    group_data[GROUP] = groups
-    group_stream: DataStream = DataStream(group_data)
+    groups_df = pretty.select(pl.col(SERIAL), pl.col(NAME)).with_columns(
+        pl.Series(GROUP, groups).alias(GROUP)
+    )
 
-    result = group_stream.to_excel(group_data_path)
+    result = write(groups_df, groups_path)
     if result.is_err():
         return result.propagate()
 
-    groups_arr: np.ndarray = np.array(groups)
+    for group in range(1, num_groups + 1):
+        group_df = groups_df.filter(pl.col(GROUP) == group)
+        group_path: Path = get_group_path(group)
 
-    for grp_num in range(1, num_groups + 1):
-        mask: np.ndarray = cast(np.ndarray, groups_arr == grp_num)
-        grp_num_data: pl.DataFrame = group_data.loc[mask, :]
-        grp_num_stream: DataStream = DataStream(grp_num_data)
-        grp_num_path: Path = get_group_path(grp_num)
-
-        result = grp_num_stream.to_excel(grp_num_path)
+        result = write(group_df, group_path)
         if result.is_err():
             return result.propagate()
 
