b4a50183694094d1a295493b8440032c0a231851
chijiokejoseph <chijiokejoseph7@proton.me>
2026-01-21 11:50:10 +0100

    fix bug in 'clean_name' utility
    This commit adapts a change from a similar commit in the main branch to the current branch

--- src/pylms/clean/clean_name.py
+++ src/pylms/clean/clean_name.py
@@ -42,9 +42,16 @@ def _clean_name(entry: str) -> str:
     Returns:
         str: The formatted name string.
     """
+    entry = entry.strip()
     if entry.find(COMMA_DELIM) == -1:
-        entries: list[str] = entry.split(SPACE_DELIM)
-        entry = COMMA_DELIM.join(entries)
+        delim = SPACE_DELIM
+    else:
+        delim = COMMA_DELIM
+
+    entries = entry.split(delim)
+    entries = [entry.title() for entry in entries]
+    entry = COMMA_DELIM.join(entries)
+
     entry = entry.title()
 
     idx = entry.find(ARABIC_APOSTROPHE)
