6aa418aad3f28119c7cecef4d1b5530562a259a8
chijiokejoseph <chijiokejoseph7@proton.me>
2026-01-23 19:14:47 +0100

    update stale API in 'append_update' utility of 'data_service' package
    This commit updates some old codes which relied on metadata stored across folders which have now been moved into a single 'history.json' file for tracking application history on rerun.

--- src/pylms/data_service/append.py
+++ src/pylms/data_service/append.py
@@ -1,8 +1,7 @@
 from ..data import DataStore, DataStream
-from ..date import det_week_num
-from ..errors import Result, Unit, eprint
+from ..errors import Result, Unit
+from ..info import print_info
 from ..models import UpdateFormInfo
-from ..paths import ret_update_path
 from ..preprocess import clean_new_data
 from .add import add
 
@@ -10,27 +9,21 @@ from .add import add
 def append_update(
     ds: DataStore, update_stream: DataStream, info: UpdateFormInfo
 ) -> Result[Unit]:
-    week_num: int = det_week_num()
-    update_form_path, update_record_path = ret_update_path(info.timestamp)
+    week_num = info.week_num
 
-    if update_record_path.exists():
-        msg = f"Entries in the Data Form whose metadata is located at {update_form_path.resolve()} have already been appended."
-        eprint(msg)
-        return Result.err(msg)
+    new_ds = clean_new_data(update_stream)
+    if new_ds.is_err():
+        return new_ds.propagate()
 
-    ds_result: Result[DataStore] = clean_new_data(update_stream)
-    if ds_result.is_err():
-        msg = f"Error cleaning new data: {ds_result.unwrap_err()}"
-        eprint(msg)
-        return Result.err(msg)
-    ds_to_add: DataStore = ds_result.unwrap()
-    new_ds = add(ds, ds_to_add)
+    new_ds = new_ds.unwrap()
+    new_ds = add(new_ds, new_ds)
 
     if new_ds.is_err():
         return new_ds.propagate()
 
     new_ds = new_ds.unwrap()
 
-    print(f"Entries retrieved from the Data Form for Week {week_num} have been saved")
-    ds.copy_from(new_ds)
-    return Result.unit()
+    print_info(
+        f"Entries retrieved from the Data Form for Week {week_num} have been saved"
+    )
+    return ds.copy_from(new_ds)
