3c2a861aee65b5e69bb741fa9601e3f12dd2daef
chijiokejoseph <chijiokejoseph7@proton.me>
2026-03-22 21:50:26 +0100

    chore: improve robustness of 'clean_attendance' utility
    This commit implements improvements to the 'clean_attendance' utility such as identifying dates that have been manually edited by confirming that their records contain only 'No Class' and 'CDS' or 'Present' and 'CDS' pairs and then replicating the edited result across that date column

--- src/pylms/clean/clean_attendance.py
+++ src/pylms/clean/clean_attendance.py
@@ -6,6 +6,12 @@ from ..constants import SPACE_DELIM
 from ..record import RecordStatus
 
 
+def check_for_edit_all(col: str, record: RecordStatus) -> pl.Expr:
+    positive_case = pl.col(col) == str(record)
+    otherwise = pl.col(col) == str(RecordStatus.CDS)
+    return (positive_case | otherwise).any()
+
+
 def clean_attendance(data: pl.DataFrame) -> pl.DataFrame:
     """Clean date columns by standardizing attendance status values.
 
@@ -16,19 +22,24 @@ def clean_attendance(data: pl.DataFrame) -> pl.DataFrame:
         pl.DataFrame: DataFrame with cleaned date columns.
     """
     reg = re.compile(r"^\d{2}/\d{2}/\d{4}$")
-    data = data.with_columns(
-        [
-            pl.when(pl.col(column).unique().is_in([str(RecordStatus.NO_CLASS)]))
+    date_cols = [col for col in data.columns if reg.fullmatch(col) is not None]
+    for column in date_cols:
+        data = data.with_columns(
+            pl.when(check_for_edit_all(column, RecordStatus.NO_CLASS))
             .then(pl.lit(str(RecordStatus.NO_CLASS)).alias(column))
             .otherwise(
-                pl.when(pl.col(column).is_in([str(RecordStatus.PRESENT)])).then(
+                pl.when(check_for_edit_all(column, RecordStatus.PRESENT))
+                .then(
                     pl.when(pl.col(column) == SPACE_DELIM)
                     .then(pl.lit(RecordStatus.PRESENT))
-                    .otherwise(pl.col(column))
+                    .otherwise(
+                        pl.when(pl.col(column) == SPACE_DELIM)
+                        .then(pl.lit(str(RecordStatus.ABSENT)))
+                        .otherwise(pl.col(column))
+                    )
                 )
+                .otherwise(pl.col(column))
             )
-            for column in data.columns
-            if reg.fullmatch(column) is not None
-        ]
-    )
+            .alias(column)
+        )
     return data
