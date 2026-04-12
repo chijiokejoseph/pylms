ccc2fdf8ef565d50dffe5bc265849ba7aaa09a5f
chijiokejoseph <chijiokejoseph7@proton.me>
2026-04-12 15:36:39 +0100

    chore: refactor cleaning function for attendance for student data for update to 'clean' package
    This commit moves a cleaning logic for cleaning attendance on a new student data being used to update the main student datastore from the 'data_service' package where it existed to a distinct 'clean_attendance' function exposed under the 'clean' package

--- src/pylms/clean/clean_attendance.py
+++ src/pylms/clean/clean_attendance.py
@@ -1,18 +1,17 @@
-import re
-
 import polars as pl
 
 from ..constants import SPACE_DELIM
+from ..history import History, get_held_classes
 from ..record import RecordStatus
 
 
 def check_for_edit_all(col: str, record: RecordStatus) -> pl.Expr:
     positive_case = pl.col(col) == str(record)
     otherwise = pl.col(col) == str(RecordStatus.CDS)
-    return (positive_case | otherwise).any()
+    return (positive_case | otherwise).all()
 
 
-def clean_attendance(data: pl.DataFrame) -> pl.DataFrame:
+def clean_attendance(history: History, data: pl.DataFrame) -> pl.DataFrame:
     """Clean date columns by standardizing attendance status values.
 
     Args:
@@ -21,24 +20,23 @@ def clean_attendance(data: pl.DataFrame) -> pl.DataFrame:
     Returns:
         pl.DataFrame: DataFrame with cleaned date columns.
     """
-    reg = re.compile(r"^\d{2}/\d{2}/\d{4}$")
-    date_cols = [col for col in data.columns if reg.fullmatch(col) is not None]
-    for column in date_cols:
+    held_dates = get_held_classes(history, "")
+    for column in held_dates:
         data = data.with_columns(
             pl.when(check_for_edit_all(column, RecordStatus.NO_CLASS))
             .then(pl.lit(str(RecordStatus.NO_CLASS)).alias(column))
             .otherwise(
                 pl.when(check_for_edit_all(column, RecordStatus.PRESENT))
                 .then(
-                    pl.when(pl.col(column) == SPACE_DELIM)
-                    .then(pl.lit(RecordStatus.PRESENT))
-                    .otherwise(
-                        pl.when(pl.col(column) == SPACE_DELIM)
-                        .then(pl.lit(str(RecordStatus.ABSENT)))
-                        .otherwise(pl.col(column))
-                    )
+                    pl.when(pl.col(column) != str(RecordStatus.CDS))
+                    .then(pl.lit(str(RecordStatus.PRESENT)).alias(column))
+                    .otherwise(pl.col(column))
                 )
-                .otherwise(pl.col(column))
+                .otherwise(
+                    pl.when(pl.col(column) == SPACE_DELIM)
+                    .then(pl.lit(str(RecordStatus.ABSENT)))
+                    .otherwise(pl.col(column))
+                )
             )
             .alias(column)
         )
